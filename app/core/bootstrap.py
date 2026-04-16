d3a5b0f9785deb0c4e9e2e04bdfab434875835be
Chiến Nguyễn <zzchien96zz@gmail.com>
2026-04-16 13:39:04 +0700

    init local object database. (#3)
    Co-authored-by: Vis10n <hiroshikc96@gmail.com>

--- app/core/bootstrap.py
+++ app/core/bootstrap.py
@@ -3,7 +3,16 @@ import logging
 from fastapi import FastAPI
 from app.api.router import root_router
 
+from data.db import create_db_and_tables
+from contextlib import asynccontextmanager
+
 log = logging.getLogger(__name__)
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await create_db_and_tables()
+    yield
 
 
 def bootstrap() -> FastAPI:
@@ -14,7 +23,7 @@ def bootstrap() -> FastAPI:
 def create_app() -> FastAPI:
     log.info("Starting app...")
 
-    app = FastAPI()
+    app = FastAPI(lifespan=lifespan)
     app.include_router(root_router)
 
     return app
